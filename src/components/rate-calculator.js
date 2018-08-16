1a1aab1e35199431d6de69b5568faa10643f6a5c
Reed Bertolette <reed@bertopro.com>
2018-08-15 16:19:21 -1000

    my solution with dynamic setState

--- src/components/rate-calculator.js
+++ src/components/rate-calculator.js
@@ -6,16 +6,26 @@ import Output from './output';
 export default class RateCalculator extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            dayrate: 500,
+            hours: 8
+        }
+    }
+
+    changeState(val, which) {
+        this.setState({
+            [which]: val
+        });
+
     }
 
     render() {
-        const rate = 10;
+        const rate =  this.state.dayrate / this.state.hours;
         return (
             <form>
-                <NumberInput id="day-rate" label="Day rate" min={0} max={5000} />
-                <NumberInput id="hours" label="Hours" min={1} max={12} />
-                <Output id="hourly-rate" label="Hourly rate" value={rate.toFixed(2)}
-/>
+                <NumberInput onChange={value => this.changeState(value, 'dayrate')} id="day-rate" label="Day rate" min={0} max={5000} value={this.state.dayrate} />
+                <NumberInput onChange={value => this.changeState(value, 'hours')} id="hours" label="Hours" min={1} max={12} value={this.state.hours} />
+                <Output id="hourly-rate" label="Hourly rate" value={rate.toFixed(2)} />
             </form>
         );
     }
